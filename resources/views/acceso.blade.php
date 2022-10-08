ce46ef97921ef9141bd869fe72aa8f67a96c9c03
Osvaldo Rene Rojo Roa <osvaldo.rojo@zapotlanejo.tecmm.edu.mx>
2022-10-08 16:58:13 -0500

    Mejoras al sistema

--- resources/views/acceso.blade.php
+++ resources/views/acceso.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('titulo')
-    Segunda Pagina
+    Dashboard
 @endsection
 @section('contenido')
 <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
    <section class="container-fluid mx-auto mt-10>
         <div>
-            @csrf
-            <p>{{auth()->user()->name}}</p>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">DASHBOARD </h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{auth()->user()->name}}</h1>
         </div>
     </section>
    
@@ -32,91 +32,98 @@
                 </th>
             </tr>
         </thead>
+        
         @foreach ($user as $dato)
-        <tbody>
-            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{$dato->id}}
-                </th>
-                <td class="py-4 px-6">
-                    {{$dato->name}}
-                </td>
-                <td class="py-4 px-6">
-                    {{$dato->email}}
-                </td>
-                <td class="py-4 px-6">
-                    <!--sweetalert crear-->
-                   @if (session('modificado'))
-                   @push('scripts')
-                     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                      <script>
-                          Swal.fire({
-                       position: 'top-end',
-                       icon: 'success',
-                       title: 'Elemento Modificado',
-                       showConfirmButton: false,
-                       timer: 1500
-                     })
-                      </script>
-                    @endpush    
-                @endif
-                  <!--fin sweetalert crear-->
-                    <form action="{{route('post.modificar',$dato->id)}}" method="POST">
-                        @csrf
-                    <button
-                        type="submit" 
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                            Editar
-                    </button>
-                    </form>
-                 
-                </td>
-                <td class="py-4 px-6">
-                   <form action="{{route('post.destroy',$dato->id)}}" method="POST">
-                   <!--sweetalert crear-->
-                   @if (session('creado'))
+         @if ($dato->email===auth()->user()->email)
+             
+         @else
+             
+         
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{$dato->id}}
+                    </th>
+                    <td class="py-4 px-6">
+                        {{$dato->name}}
+                    </td>
+                    <td class="py-4 px-6">
+                        {{$dato->email}}
+                    </td>
+                    <td class="py-4 px-6">
+                        <!--sweetalert crear-->
+                    @if (session('modificado'))
                     @push('scripts')
-                      <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                       <script>
-                           Swal.fire({
+                        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                        <script>
+                            Swal.fire({
                         position: 'top-end',
                         icon: 'success',
-                        title: 'Elemento Creado',
+                        title: 'Elemento Modificado',
                         showConfirmButton: false,
                         timer: 1500
-                      })
-                       </script>
-                     @endpush    
-                 @endif
-                   <!--fin sweetalert crear-->
-                    <!--inicio del sweetalert Eliminar-->
-                    @if (session('mensaje'))
-                    @push('scripts')
-                      <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                       <script>
-                           Swal.fire({
-                        position: 'top-end',
-                        icon: 'warning',
-                        title: 'Elemento Eliminado',
-                        showConfirmButton: false,
-                        timer: 1500
-                    })
-                       </script>
-                     @endpush    
-                 @endif
-                    <!--fin del sweetalert-->
-                            @method('DELETE') 
+                        })
+                        </script>
+                        @endpush    
+                    @endif
+                    <!--fin sweetalert crear-->
+                        <form action="{{route('post.modificar',$dato->id)}}" method="POST">
                             @csrf
-                        <button 
+                        <button
                             type="submit" 
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                Eliminar
+                                Editar
                         </button>
-                  </form>
-                </td>
-            </tr>
-          
-        </tbody>
+                        </form>
+                    
+                    </td>
+                    <td class="py-4 px-6">
+                    <form action="{{route('post.destroy',$dato->id)}}" method="POST">
+                    <!--sweetalert crear-->
+                    @if (session('creado'))
+                        @push('scripts')
+                        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                        <script>
+                            Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Elemento Creado',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        </script>
+                        @endpush    
+                    @endif
+                    <!--fin sweetalert crear-->
+                        <!--inicio del sweetalert Eliminar-->
+                        @if (session('mensaje'))
+                        @push('scripts')
+                        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                        <script>
+                            Swal.fire({
+                            position: 'top-end',
+                            icon: 'warning',
+                            title: 'Elemento Eliminado',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        </script>
+                        @endpush    
+                    @endif
+                        <!--fin del sweetalert-->
+                                @method('DELETE') 
+                                @csrf
+                            <button 
+                                type="submit" 
+                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Eliminar
+                            </button>
+                    </form>
+                    </td>
+                </tr>
+            
+            </tbody>
+            @endif  
         @endforeach
      </table> 
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,24 +11,41 @@
 <header class="p-5 border-b bg-white shadow">
   <div class="container mx-auto flex justify-between p-2">
      <h1 class="text-3xl font-black">
-        Practica con Tailwind
+        Laravel con Tailwind
      </h1>
      @auth
         <nav class="flex gap-2 items-center">
-          <a class="font-bold  text-gray-600 text-sm">
-            <spam>
-              {{auth()->user()->email}}
-            </spam>
-          </a>
-        <!--  <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">
-            Cerrar Sesion
-        </a>-->
-        <form method="POST" action="{{route('logout')}}">
-          @csrf
-            <button type="submit" class="font-bold uppercase text-gray-600 text-sm">
-              Cerrar Sesion
-            </button>
-        </form>
+           <!-- Profile dropdown -->
+           <div class="relative ml-3">
+            <div>
+              <a  class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                <span class="sr-only">Open user menu</span>
+                <img class="h-8 w-8 rounded-full" src="{{asset('imagenes/login.png')}}" alt="">
+              </a>
+            </div>
+
+            <!--
+              Dropdown menu, show/hide based on menu state.
+
+              Entering: "transition ease-out duration-100"
+                From: "transform opacity-0 scale-95"
+                To: "transform opacity-100 scale-100"
+              Leaving: "transition ease-in duration-75"
+                From: "transform opacity-100 scale-100"
+                To: "transform opacity-0 scale-95"
+            -->
+            <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+              <!-- Active: "bg-gray-100", Not Active: "" -->
+              <a class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">{{auth()->user()->email}}</a>
+              <a class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">
+              <form method="POST" action="{{route('logout')}}">
+                @csrf
+                  <button type="submit" class="font-bold uppercase text-gray-600 text-sm">
+                    Cerrar Sesion
+                  </button>
+              </form>
+              </a>
+            </div>
         </nav>
      @endauth
         
